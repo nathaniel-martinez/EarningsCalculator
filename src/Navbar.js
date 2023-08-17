335d30900af957cd572c7f1cdc28fef59330f9d4
Nathaniel Martinez <nathanielwork145@gmail.com>
2023-08-17 03:14:47 -0400

    Finshed HomePage

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,9 +3,15 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
 	  <div className="container">
-	  	<div className="row">
-	  		<div className="col-sm"><Link to="/calculate">Calculate Earnings</Link></div>
-	  		<div className="col-sm"><Link to="/compare">Compare Earnings</Link></div>
+	  	<div className="row bg-light">
+	  		<div className="col-sm border view overlay">
+	  			<Link to="/calculate" className="text-decoration-none reset-color">Calculate Earnings</Link>
+	  			<div className="mask flex-center border rgba-blue-light"></div>
+	  		</div>
+	  		<div className="col-sm view overlay">
+	  			<Link to="/compare" className="text-decoration-none reset-color">Compare Earnings</Link>
+	  			<div className="mask flex-center border rgba-blue-light"></div>
+	  		</div>
 	  	</div>
 	  </div>
   );
